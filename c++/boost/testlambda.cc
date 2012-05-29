b1a561abdd45b8ff64a7dea1c33802412709869d
zhangjun <zhangjun@zhangjun-home-ubuntu.(none)>
2012-05-29 22:27:04 +0800

    fix error

--- c++/boost/testlambda.cc
+++ c++/boost/testlambda.cc
@@ -12,59 +12,63 @@ using namespace boost::lambda;
 template <typename T>
 ostream & operator<<(ostream &os, const list<T>& v )  
 {  
-	os<<"[";
+    os<<"[";
     BOOST_FOREACH(T i, v)
     {
-		os<<i<<",";
+        os<<i<<",";
     }  
 
-	os<<"]";
+    os<<"]";
     return os;  
 } 
 
 int add(int n)
 {
-	return n+1;
+    return n+1;
 }
 
 class A {
-  	int i; 
-	mutable int j;
+    int i; 
+    mutable int j;
 public:
- 	A(int ii, int jj) 
+    A(int ii, int jj) 
 	:i(ii), j(jj) {};
   
-	void set_i(int x) { i = x; }; 
-  	
-	void set_j(int x) const { j = x; }; 
+    void set_i(int x) { i = x; }; 
+    
+    void set_j(int x) const { j = x; }; 
 };
 
 
 int main()
 {
-	list<int> v(10);
-	int i=0;
-	for_each(v.begin(), v.end(), _1 = 1);
-	cout<<v<<endl;
+    list<int> v(10);
+    int i=0;
+    for_each(v.begin(), v.end(), _1 = 1);
+    cout<<v<<endl;
 
-	for_each(v.begin(), v.end(), _1 = bind(add,_1));
-	cout<<v<<endl;
+    for_each(v.begin(), v.end(), _1 = bind(add,_1));
+    cout<<v<<endl;
 
-	for_each(v.begin(), v.end(), cout<<_1);
-	cout<<endl;
+    for_each(v.begin(), v.end(), cout<<_1);
+    cout<<endl;
 	
-	A a(0,0); 
-	int k = 1;
+    A a(0,0); 
+    int k = 1;
 
-	bind(&A::set_i, boost::ref(a), _1)(k);
-	bind(&A::set_j, a, _1)(k);
+    bind(&A::set_i, boost::ref(a), _1)(k);
+    bind(&A::set_j, a, _1)(k);
 
-	i = 20; 
-	int j;
-	for_each(v.begin(), v.end(), (var(j) = _1, _1 = var(i), var(i) = var(j))); 	
-	cout<<v<<endl;
-	cout<<i<<endl;
+    i = 20; 
+    int j;
+    for_each(v.begin(), v.end(), ( var(j) = 100 )); 	
+    cout<<i<<endl;
+    cout<<j<<endl;
 
-	return 0;
+    int index=0;
+    for_each(v.begin(), v.end(), (cout << ++var(index) << ':' << _1 << ';'));
+    cout << endl;
+
+    return 0;
 }
 
