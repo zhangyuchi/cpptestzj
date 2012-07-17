361d4c8c3ccd7ee4c6cc943251bb1d492ae17db9
zhangjun-s <zhangjun-s@build8.kill.corp.qihoo.net>
2012-07-17 16:53:32 +0800

    update

--- c++/testvector.cc
+++ c++/testvector.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <boost/foreach.hpp>
+#include <string.h>
+#include <cstdio>
 
 using namespace std;
 
 template <typename T>
 void print_vector(const vector<T> &v)
 {
+    cout<<v.size()<<endl;
 	BOOST_FOREACH(T i, v)
     {
         cout<<i<<" ";
@@ -15,8 +18,20 @@ void print_vector(const vector<T> &v)
 	cout<<endl;
 }
 
+void print_vector(const vector<unsigned char> &v)
+{
+    printf("%d\n", v.size());
+	BOOST_FOREACH(unsigned char i, v)
+    {
+        printf("%d-%c ", i,i);
+    } 
+
+	cout<<endl;
+}
+
 int main()
 {
+    std::cout<<"------------------------------------------------------"<<std::endl;
 	vector<int> Iv;
 
 	for(int i = 0; i<10; i++)
@@ -31,6 +46,25 @@ int main()
 	print_vector(Iv);
 
 	cout<<"done"<<endl;
+    std::cout<<"------------------------------------------------------"<<std::endl;
+
+    unsigned char* buf = (unsigned char *)"0123456789";
+    vector<unsigned char> *pvstr = new vector<unsigned char>();
+    int len = 16;
+    buf = (unsigned char *)&len;
+    pvstr->insert(pvstr->end(), buf, buf+4);
+    pvstr->insert(pvstr->end(), buf+4, buf+7);
+    pvstr->insert(pvstr->end(), buf+7, buf+10);
+    print_vector(*pvstr);
+
+    len = 32;
+    memcpy(&(*pvstr)[4], &len,4);
+    print_vector(*pvstr);
+
+    std::vector<uint8_t> ka;
+    uint8_t kamsg[] = "keepalive";
+    ka.insert(ka.begin(), kamsg, kamsg+strlen((char *)kamsg));
+    print_vector(ka);
 
 	return 0;
 }
