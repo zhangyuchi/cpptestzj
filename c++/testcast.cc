a477ada03f3083b59c24f06c524206015e944301
zhangjun-s <zhangjun-s@build8.kill.corp.qihoo.net>
2012-10-29 15:12:10 +0800

    change

--- c++/testcast.cc
+++ c++/testcast.cc
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
 	int n=9;
-	double d = reinterpret_cast<double> (n);
+	//double d = reinterpret_cast<double> (n);
+	double d = static_cast<double> (n);
 	long l = static_cast<long> (n);
 	
 	cout << n << " -> " << d << " -> " << l << endl;
