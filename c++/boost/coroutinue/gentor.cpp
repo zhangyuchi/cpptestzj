b4274b0a6e7fc3d8e10faef9e513d80974e6c487
zhangjun-s <zhangjun-s@build9.safe.corp.qihoo.net>
2013-02-27 19:16:02 +0800

    add

---
+++ c++/boost/coroutinue/gentor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include <boost/bind.hpp>
+#include <boost/coroutine/all.hpp>
+
+typedef boost::coroutines::coroutine< int() >       coro_t;
+typedef boost::range_iterator< coro_t >::type       iterator_t;
+
+void power( coro_t::caller_type & ca, int number, int exponent)
+{
+    int counter = 0;
+    int result = 1;
+    while ( counter++ < exponent)
+    {
+        result = result * number;
+        ca( result);
+    }
+}
+
+int main()
+{
+    coro_t c( boost::bind( power, _1, 2, 8) );
+    iterator_t e( boost::end( c) );
+    for ( iterator_t i( boost::begin( c) ); i != e; ++i)
+        std::cout << * i <<  " ";
+
+    std::cout << "\nDone" << std::endl;
+
+    return EXIT_SUCCESS;
+}
