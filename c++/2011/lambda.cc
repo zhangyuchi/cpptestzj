e862f43edf64edb8aad0810217ff0ad1e91ed0f7
zhangjun <zhangyuchi@gmail.com>
2013-06-18 10:38:31 +0800

    change

--- c++/2011/lambda.cc
+++ c++/2011/lambda.cc
@@ -10,9 +10,19 @@ void test()
     z();
 }
 
+void test1()
+{
+    int x = 4;
+    int y = 5;
+    [&](){x = 2;y = 2;}();
+    [=]() mutable{x = 3;y = 5;}();
+    [=,&x]() mutable{x = 7;y = 9;}();
+}
+
 int main()
 {
     test();
+    test1();
     return 0;    
 }
 
--- c++/2011/test.cpp
+++ c++/2011/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -7,12 +8,26 @@ constexpr int Fib(int n)
     return n<=2 ? 1 : Fib(n-1)+Fib(n-2);
 }
 
+template<class T, class U>
+auto add(T x, U y) -> decltype(x+y)
+{
+    return x+y;
+}
+
+
 int main()
 {
-    cout << Fib(45) << endl;
+    cout << Fib(25) << endl;
 
     //int a = 15;
     //cout<<Fib(a);
+
+    uint8_t *p = nullptr;
+
+    string test=R"(C:\A\B\C\D\file1.txt)";
+    cout << test << endl;
+
+    cout << add(1,1L) <<endl;    
 
     return 0;
 }
