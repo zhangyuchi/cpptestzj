cd040d6bdcc144faeedb79f371e7663558b9c945
zhangjun <zhangyuchi@gmail.com>
2013-10-25 10:47:30 +0800

    Create newlist.c

---
+++ newlist.c
@@ -0,0 +1,14 @@
+void remove_if(node ** head, remove_fn rm)
+{
+    for (node** curr = head; *curr; )
+    {
+        node * entry = *curr;
+        if (rm(entry))
+        {
+            *curr = entry->next;
+            free(entry);
+        }
+        else
+            curr = &entry->next;
+    }
+}
