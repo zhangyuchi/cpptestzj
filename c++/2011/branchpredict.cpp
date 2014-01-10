708409f6cc352820f7b550514cd427aa72a55180
zhangjun-s <zhangjun-s@build10.kill.corp.qihoo.net>
2014-01-10 16:29:13 +0800

    add branchpredict

---
+++ c++/2011/branchpredict.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <ctime>
+#include <iostream>
+
+int main()
+{
+    // Generate data
+    const unsigned arraySize = 4096;//32768;
+    int data[arraySize];
+
+    for (unsigned c = 0; c < arraySize; ++c)
+        data[c] = std::rand() % 256;
+
+    // !!! With this, the next loop runs faster
+    // GCC 4.6.1 with -O3 or -ftree-vectorize on x64 is able to generate a conditional move. 
+    // So there is no difference between the sorted and unsorted data - both are fast.
+    
+    std::sort(data, data + arraySize);
+
+    // Test
+    clock_t start = clock();
+    long long sum = 0;
+
+    for (unsigned i = 0; i < 100000; ++i)
+    {
+        // Primary loop
+        for (unsigned c = 0; c < arraySize; ++c)
+        {
+            if (data[c] >= 128)
+                sum += data[c];
+        }
+    }
+
+    double elapsedTime = static_cast<double>(clock() - start) / CLOCKS_PER_SEC;
+
+    std::cout << elapsedTime << std::endl;
+    std::cout << "sum = " << sum << std::endl;
+}
