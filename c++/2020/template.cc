a32559871f1775d35673ca8eab303d41fea296fb
zhangjun <zhangjun06@qianxin.com>
2022-03-24 10:10:28 +0800

    up

--- c++/2020/template.cc
+++ c++/2020/template.cc
@@ -1,16 +1,22 @@
     #include <type_traits>
     #include <iostream>
 
-    template <typename CharT>
-    std::enable_if_t<sizeof(CharT) == 1> create(const CharT * data, size_t size)
-    {
-        std::cout<<"create for "<<sizeof(CharT)<<std::endl;
-    }
+    // template <typename CharT>
+    // std::enable_if_t<sizeof(CharT) == 1> create(const CharT * data, size_t size)
+    // {
+    //     std::cout<<"create for "<<sizeof(CharT)<<std::endl;
+    // }
 
     template <typename CharT>
     std::enable_if_t<sizeof(CharT) == 2> create(const CharT * data, size_t size)
     {
         std::cout<<"create for "<<sizeof(CharT)<<std::endl;
+    }
+
+    template <typename CharT, std::enable_if_t<sizeof(CharT) == 1, bool> = true>
+    void create(const CharT * data, size_t size)
+    {
+        std::cout<<"create2 for "<<sizeof(CharT)<<std::endl;
     }
 
     int main() {
